74cdedb0b9e3ecdc2ca4b003b29ac7d0766a58ff
kni9ht <55698799+kni9ht@users.noreply.github.com>
2021-02-22 12:27:05 +0530

    Update urls.py

--- newsPortal/urls.py
+++ newsPortal/urls.py
@@ -27,9 +27,5 @@ urlpatterns = [
     path('news/', include('news.urls')),
     path('home', view.web),
     path('load', view.load),
-    path('link', view.link),
-    # url(r'^media/(?P<path>.*)$', serve,
-    #     {'document_root':       settings.MEDIA_ROOT}),
-    # url(r'^static/(?P<path>.*)$', serve,
-    #     {'document_root': settings.STATIC_ROOT}),
+    path('link', view.link)
 ]
