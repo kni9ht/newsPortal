8550497db50fbbb52285e12f6f0f5a1d2d95a52b
kni9ht <55698799+kni9ht@users.noreply.github.com>
2021-02-22 13:08:43 +0530

    Update settings.py

--- newsPortal/settings.py
+++ newsPortal/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '4&o_-!v6!)u_)bkkxc$)sq!skz!(2egh1wly83v_^n8^i$#%5&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['kni9ht.herokuapp.com','127.0.0.1' ]
 
