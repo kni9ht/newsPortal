7089a7b522bf1a55a71eaa428fd0de2c539eec76
kni9ht <55698799+kni9ht@users.noreply.github.com>
2021-02-22 12:30:32 +0530

    Update urls.py

--- news/urls.py
+++ news/urls.py
@@ -5,7 +5,5 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('', views.index),
-    path('query/', views.query),
-     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    path('query/', views.query)
 ]
